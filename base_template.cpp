8d373cb6cc365cacb1fe624a359ca6759b5a93af
AliiAhmadi <aliahmadi.github@gmail.com>
2024-01-06 08:30:50 +0330

    complete BigInt struct

--- base_template.cpp
+++ base_template.cpp
@@ -367,6 +367,153 @@ struct BigInt
     {
         return a / gcd(a, b) * b;
     }
+
+    void read(const std::string &str)
+    {
+        this->sign = 1;
+        this->digits.clear();
+        int pos = 0;
+        while (pos < (int)str.size() && (str[pos] == '-' || str[pos] == '+'))
+        {
+            if (str[pos] == '-')
+                this->sign = -this->sign;
+            pos++;
+        }
+
+        for (int i = str.size() - 1; i >= pos; i -= BASE_DIGITS)
+        {
+            int x = 0;
+            for (int j = std::max(pos, i - BASE_DIGITS + 1); j <= i; j++)
+                x = (x * 10) + str[j] - '0';
+            this->digits.push_back(x);
+        }
+
+        this->trim();
+    }
+
+    friend std::istream &operator>>(std::istream &stream, BigInt &bg)
+    {
+        std::string s;
+        stream >> s;
+        bg.read(s);
+        return stream;
+    }
+
+    friend std::ostream &operator<<(std::ostream &stream, const BigInt &bg)
+    {
+        if (bg.sign == -1)
+            stream << '-';
+        stream << (bg.digits.empty() ? 0 : bg.digits.back());
+        for (int i = (int)bg.digits.size() - 2; i >= 0; i--)
+            stream << std::setw(BASE_DIGITS) << std::setfill('0') << bg.digits[i];
+        return stream;
+    }
+
+    static std::vector<int> convert_base(const std::vector<int> &a, int old_digits, int new_digits)
+    {
+        std::vector<long long> p(std::max(old_digits, new_digits) + 1);
+        p[0] = 1;
+        for (int i = 0; i < (int)p.size(); i++)
+            p[i] = p[i - 1] * 10;
+        std::vector<int> result;
+        long long cur = 0;
+        int cur_digits = 0;
+        for (int i = 0; i < (int)a.size(); i++)
+        {
+            cur += a[i] * p[cur_digits];
+            cur_digits += old_digits;
+            while (cur_digits >= new_digits)
+            {
+                result.push_back(int(cur % p[new_digits]));
+                cur /= p[new_digits];
+                cur_digits -= new_digits;
+            }
+        }
+        result.push_back((int)cur);
+        while (!result.empty() && !result.back())
+            result.pop_back();
+        return result;
+    }
+
+    BigInt operator*(const BigInt &bg) const
+    {
+        std::vector<int> a6 = convert_base(this->digits, BASE_DIGITS, 6);
+        std::vector<int> b6 = convert_base(bg.digits, BASE_DIGITS, 6);
+
+        std::vector<long long> a(a6.begin(), a6.end());
+        std::vector<long long> b(b6.begin(), b6.end());
+
+        while (a.size() < b.size())
+            a.push_back(0);
+        while (b.size() < a.size())
+            b.push_back(0);
+        while (a.size() & (a.size() - 1))
+            a.push_back(0), b.push_back(0);
+
+        std::vector<long long> c = karatsubaMultiply(a, b);
+        BigInt result;
+        result.sign = this->sign * bg.sign;
+
+        for (int i = 0, carry = 0; i < (int)c.size(); i++)
+        {
+            long long cur = c[i] + carry;
+            result.digits.push_back((int)(cur % 1000000));
+            carry = (int)(cur / 1000000);
+        }
+
+        result.digits = convert_base(result.digits, 6, BASE_DIGITS);
+        result.trim();
+        return result;
+    }
+
+    static std::vector<long long> karatsubaMultiply(
+        const std::vector<long long> &a,
+        const std::vector<long long> &b)
+    {
+        int n = a.size();
+        std::vector<long long> result(n + n);
+        if (n <= 32)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result[i + j] += a[i] + b[j];
+                }
+            }
+            return result;
+        }
+
+        int k = n >> 1;
+        std::vector<long long> a1(a.begin(), a.begin() + k);
+        std::vector<long long> a2(a.begin() + k, a.end());
+        std::vector<long long> b1(b.begin(), b.begin() + k);
+        std::vector<long long> b2(b.begin() + k, b.end());
+
+        std::vector<long long> a1b1 = karatsubaMultiply(a1, b1);
+        std::vector<long long> a2b2 = karatsubaMultiply(a2, b2);
+
+        for (int i = 0; i < k; i++)
+        {
+            a2[i] += a1[i];
+            b2[i] += b1[i];
+        }
+
+        std::vector<long long> r = karatsubaMultiply(a2, b2);
+
+        for (int i = 0; i < (int)a1b1.size(); i++)
+            r[i] -= a1b1[i];
+        for (int i = 0; i < (int)a2b2.size(); i++)
+            r[i] -= a2b2[i];
+        for (int i = 0; i < (int)r.size(); i++)
+            result[i + k] = r[i];
+        for (int i = 0; i < (int)a1b1.size(); i++)
+            result[i] += a1b1[i];
+        for (int i = 0; i < (int)a2b2.size(); i++)
+            result[i + n] += a2b2[i];
+
+        return result;
+    }
 };
 
 /*
